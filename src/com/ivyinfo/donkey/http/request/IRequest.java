676929c63511d4115efdcc3666f033464cc3b448
huuguanghui <huuguanghui@hotmail.com>
2012-07-06 18:07:14 +0800

    remove useless code

--- src/com/ivyinfo/donkey/http/request/IRequest.java
+++
@@ -1,9 +0,0 @@
-package com.ivyinfo.donkey.http.request;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-public interface IRequest {
-	
-	public void execute(HttpServletRequest request, HttpServletResponse response);
-}
--- src/com/ivyinfo/donkey/sip/SIPHelper.java
+++
@@ -1,9 +0,0 @@
-package com.ivyinfo.donkey.sip;
-
-public class SIPHelper {
-
-	public static final String INVITE = "INVITE";
-	public static final String INFO = "INFO";
-	public static final String BYE = "BYE";
-
-}
